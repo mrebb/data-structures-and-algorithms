c92127802fab9a82fe833ef11b645f7f8b9c03bc
Mrebb <madhu.rebbana@gmail.com>
2018-05-19 18:02:00 -0700

    Binary Search on sorted array solution with test methods

--- Array/array_binary_search/__test__/array_binary_search.test.js
+++ Array/array_binary_search/__test__/array_binary_search.test.js
@@ -5,6 +5,10 @@ describe('Array binary search module', () => {
   it('Array binary search module returns index of search value passed in', ()=>{
     expect(search.binarySearch([4,8,15,16,23,42], 15)).toEqual(2);
   });
+  
+  it('Array binary search module returns index of search value passed for ascending sorted array', ()=>{
+    expect(search.binarySearch([42,23,17,15,9,7,6], 15)).toEqual(3);
+  });
 
   it('Array binary search module returns -1 if search value passed is not found', ()=>{
     expect(search.binarySearch([11,22,33,44,55,66,77], 90)).toEqual(-1);
