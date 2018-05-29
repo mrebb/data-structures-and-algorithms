6eb4443aacd6529675063ba10f98e12929a69c74
Mrebb <madhu.rebbana@gmail.com>
2018-05-29 12:38:50 -0700

    refactoring

--- Array/array_adjacent_product/lib/array_adjacent_product.js
+++ Array/array_adjacent_product/lib/array_adjacent_product.js
@@ -5,30 +5,31 @@ exports.largestProduct= (arr) => {
 
   for(let i=0; i<arr.length;i++){
     for(let j=0;j<arr[i].length;j++){
-      if(typeof arr[i][j] === 'string' && arr[i][j]!= ''){
+      let currentValue = arr[i][j];
+      if(typeof currentValue === 'string' && currentValue!= ''){
         return 'wrong input';
       }
       if(i-1 != -1 && j-1!== -1){
-        topLeftProduct = arr[i-1][j-1] * arr[i][j];
-        topProduct = arr[i-1][j] * arr[i][j];
-        leftProduct = arr[i][j-1] * arr[i][j];
+        topLeftProduct = arr[i-1][j-1] * currentValue;
+        topProduct = arr[i-1][j] * currentValue;
+        leftProduct = arr[i][j-1] * currentValue;
         largestProduct = Math.max(topLeftProduct,topProduct,leftProduct,largestProduct);
       }
       if(i+1 <arr.length && j+1 <arr[i].length){
-        rightProduct = arr[i][j+1] * arr[i][j];
-        bottomProduct = arr[i+1][j] * arr[i][j];
-        bottomRightProduct = arr[i+1][j+1] * arr[i][j];
+        rightProduct = arr[i][j+1] * currentValue;
+        bottomProduct = arr[i+1][j] * currentValue;
+        bottomRightProduct = arr[i+1][j+1] * currentValue;
         largestProduct = Math.max(rightProduct,bottomProduct,bottomRightProduct,largestProduct);
       }
       if(i-1 != -1 && j-1!== -1 && i+1 <arr.length && j+1 <arr[i].length){
-        topRightProduct = arr[i-1][j+1] * arr[i][j];
-        bottomLeftProduct = arr[i+1][j-1] * arr[i][j]; 
+        topRightProduct = arr[i-1][j+1] * currentValue;
+        bottomLeftProduct = arr[i+1][j-1] * currentValue; 
         largestProduct = Math.max(topRightProduct,bottomLeftProduct,largestProduct);
       }
       if(j===arr[i].length-1 && i+1 <arr.length){
-        leftProduct = arr[i][j-1] * arr[i][j];
-        bottomLeftProduct = arr[i+1][j-1] * arr[i][j]; 
-        bottomProduct = arr[i+1][j] * arr[i][j];
+        leftProduct = arr[i][j-1] * currentValue;
+        bottomLeftProduct = arr[i+1][j-1] * currentValue; 
+        bottomProduct = arr[i+1][j] * currentValue;
         largestProduct = Math.max(leftProduct,bottomLeftProduct,bottomProduct,largestProduct);
       }
     }
