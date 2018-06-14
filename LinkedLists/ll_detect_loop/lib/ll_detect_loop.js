5985c0403c6d4e1c7c164380615a54beb4506379
Mrebb <madhu.rebbana@gmail.com>
2018-06-14 11:29:27 -0700

    refactoring code

--- LinkedLists/ll_detect_loop/lib/ll_detect_loop.js
+++ LinkedLists/ll_detect_loop/lib/ll_detect_loop.js
@@ -1,7 +1,36 @@
-class SinglyLinkedList{
-  constructor(){
+class SinglyLinkedList {
+  constructor() {
     this.head = null;
     this.length = 0;
+  }
+  append(value) {
+    let node = new Node(value);
+    if (!this.head) {
+      this.head = node;
+      return node;
+    }
+    let currentNode = this.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    currentNode.next = node;
+    return this;
+  }
+  hasLoop(){
+    if (this.head === null) {
+      return false;
+    }
+    let p1 = this.head;
+    let p2 = this.head;
+    while (p1.next && p1.next.next) {
+      p1 = p1.next.next;
+      p2 = p2.next;
+  
+      if (p1 === p2) {
+        return true;
+      }
+    }
+    return false;
   }
 }
 class Node {
@@ -10,38 +39,7 @@ class Node {
     this.next = null;
   }
 }
-
-SinglyLinkedList.append = (value)=>{
-  let node = new Node(value);
-  if(!this.head){
-    this.head = node;
-    return node;
-  }
-  let currentNode = this.head;
-  while(currentNode.next){
-    currentNode = currentNode.next;
-  }
-  currentNode.next = node;
-  return this;
+module.exports = {
+  SinglyLinkedList,
+  Node,
 };
-
-SinglyLinkedList.hasLoop = (LL)=>{
-  console.log(LL);
-  if(LL===undefined){
-    return undefined;
-  }
-  let p1 = LL.head;
-  let p2 = LL.head;
-  while(p1.next.next){
-    p1 = p1.next.next;
-    p2 = p2.next;
-
-    if(p1===p2){
-      return true;
-    }
-  }
-  return false;
-  
-};
-
-module.exports = SinglyLinkedList;
