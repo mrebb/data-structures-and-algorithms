e9c3182e33720aa782c33bffa270e28a666da1d2
Mrebb <madhu.rebbana@gmail.com>
2018-08-14 10:56:18 -0700

    repeated_word solution with tests and readme

--- hashmaps/__test__/repeated_word.test.js
+++ hashmaps/__test__/repeated_word.test.js
@@ -1,12 +1,22 @@
 'use strict';
-const {
-  repeatedWord,
-} = require('../lib/repeated_word');
+const
+  lib
+ = require('../lib/repeated_word');
 
-describe('repeated word', () => {
-  it('check for repetition', ()=>{
-  let repeated = repeatedWord('summer');
-  expect(repeated).toEqual('');
+describe('find first repeated word', () => {
+  it('check for repetition with simple sentence', ()=>{
+    let repeatedWord = lib.repeatedWord('Once upon a time, there was a brave princess who...');
+    expect(repeatedWord).toEqual('a');
+  });
+  it('check for repetition with more combinations', ()=>{
+    let str = 'It was a queer, sultry summer, the summer they electrocuted the Rosenbergs, and I didn’t know what I was doing in New York...';
+    let repeatedWord = lib.repeatedWord(str);
+    expect(repeatedWord).toEqual('summer');
+  });
+  it('check for no repetition', ()=>{
+    let str = 'abc hello world';
+    let repeatedWord = lib.repeatedWord(str);
+    expect(repeatedWord).toEqual('no repetition');
   });
   
 });
--- hashmaps/lib/repeated_word.js
+++ hashmaps/lib/repeated_word.js
@@ -1,15 +1,40 @@
 'use strict';
-export default function repeatedWord(str){
+module.exports = exports = {};
+exports.repeatedWord= (str)=>{
   let arr=str.toLowerCase().split(' ');
-  let temp = arr;
+  let wordsSet=[];
+  let copyOfArray = arr;
   for(let i=0;i<arr.length;i++){
-    for(let j=0;j<temp.length;j++){
+    for(let j=0;j<copyOfArray.length;j++){
       if(j!==i){
-        if(arr[i]===temp[j]){
-          return arr[i].replace(',','');
+        if(arr[i].replace(',','').trim()===copyOfArray[j].replace(',','').trim()){
+          let key = copyOfArray[j].replace(',','').trim();
+          wordsSet.push({word:key,start:i,end:j});
         }
       }  
     }
   }
-  return 'no repetition';
+  var gapBetweenMatchedWords,raceConditionWordGap=0,result,wordFoundAtIndex = null;
+  wordsSet.forEach((obj)=>{
+    gapBetweenMatchedWords=obj.end-obj.start;
+    if(raceConditionWordGap==0){
+      raceConditionWordGap=gapBetweenMatchedWords;
+      result = obj.word;
+    }
+    else if(raceConditionWordGap!==0 && gapBetweenMatchedWords<raceConditionWordGap && gapBetweenMatchedWords>0){
+      raceConditionWordGap=Math.min(gapBetweenMatchedWords,raceConditionWordGap);
+      var wordStartedIndex= obj.start;
+      if(wordFoundAtIndex===null){
+        wordFoundAtIndex = wordStartedIndex;
+        result = obj.word;
+      }
+      else{
+        if (wordStartedIndex>wordFoundAtIndex || wordStartedIndex===wordFoundAtIndex){
+          result = obj.word;
+        }
+      }
+    }   
+  });
+  return result || 'no repetition' ;
+  
 }
